44e10c76b0690309bddcffafd91de79172c96315
ruslandoga <ruslandoga+gh@icloud.com>
2025-02-13 15:32:16 +0300

    eh?

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ichibase.MixProject do
   def project do
     [
       app: :ichibase,
-      version: "0.1.0",
+      version: "0.1.0-rc.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
