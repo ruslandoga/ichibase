9c37b88d97c044cb602e35020badd7aa0e953b3f
ruslandoga <ruslandoga+gh@icloud.com>
2026-01-03 16:29:46 +0300

    ci

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Ichi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_local_path: "plts", plt_core_path: "plts"]
     ]
   end
 
@@ -22,8 +23,7 @@ defmodule Ichi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
